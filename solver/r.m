7f051bb87dd29d982d3d577b9c8e42afe3fa99e4
Elliot Jmaes <ejb321@ic.ac.uk>
2024-07-09 20:58:42 +0100

    animation

--- solver/r.m
+++ solver/r.m
@@ -7,8 +7,8 @@ Sol1 = readmatrix('IBVP1_2eqn_2D.dat');
 Sol2 = readmatrix('IBVP2_2eqn_2D.dat');
 nx = height(x(:,1))
 ny = width(y(1,:))
-nt = 1200
-skip = 200
+nt = 2000
+skip = 10
 
 xr = max(x(:,1))
 yr = max(y(1,:))
@@ -27,51 +27,43 @@ for k = 1:skip:nt
     chr = int2str(k)
 
     f=figure(1);
-    f.Position = [0 0 500 500]
+    f.Position = [0 0 400 400]
     pcolor(x,y,Sol1(1+(k-1)*nx:nx+(k-1)*nx,1:ny))
     %pcolor(x,y,u(x,y))
     hold on
     colorbar
-    title(chr)
+    title(append('Activator at time step: ',chr))
     shading interp
-   % caxis([0 6])
+    caxis([0.95 1.05])
     colormap('cool')
+    xlabel('x axis')
+    ylabel('y axis')
     ylim([0 xr]);
     xlim([0 yr])
     exportgraphics(gcf,'testAnimated1.gif','Append',true);
     hold off
-
+    
     f=figure(2);
-    f.Position = [500 0 500 500]
+    f.Position = [500 0 400 400]
     pcolor(x,y,Sol2(1+(k-1)*nx:nx+(k-1)*nx,1:ny))
+    title(append('Substrate at time step: ',chr))
     %pcolor(x,y,u(x,y))
     hold on
     colorbar
     shading interp
-    %caxis([0.8 1.2])
+    caxis([0.98 1.02])
+    xlabel('x axis')
+    ylabel('y axis')
     ylim([0 xr]);
     xlim([0 yr])
     exportgraphics(gcf,'testAnimated2.gif','Append',true);
     hold off
 
-    f=figure(3);
-    f.Position = [1000 0 500 500]
-    surf(x,y,Sol1(1+(k-1)*nx:nx+(k-1)*nx,1:ny),'FaceColor','k','FaceAlpha',0.3)
-    hold on
-    surf(x,y,Sol2(1+(k-1)*nx:nx+(k-1)*nx,1:ny),'FaceColor','r','FaceAlpha',0.3)
-    %pcolor(x,y,u(x,y))
-    caxis([0 1])
-    ylim([0 xr]);
-    xlim([0 yr])
-    zlim([0 1])
-    hold off
 
 
-
-    pause(0.2);
+    %pause(0.05);
     clear('1')
     clear('2')
-    clear('3')
     hold off
 
     
